d7332513a867e92ce8c84feca78bc3fb9473a629
Urieel <urielsalazar.dev@gmail.com>
2026-06-24 17:45:04 -0600

    Add javascript

---
+++ dental_project/script.js
@@ -0,0 +1,14 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.navbar-menu');
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener(
+    "click",() => {
+        hamburguer.classList.remove("active");
+        navMenu.classList.remove("active");
+
+    }
+))
